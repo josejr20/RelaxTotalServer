8d51dc8f740e65eaa552a9c48a1ef62994d545be
josejr20 <jararojasjose604@gmail.com>
2026-02-28 17:11:04 -0500

    Solucionando codigo

--- Centro-de-masajes-Relax-Relax-main/backend/centromasajes/src/main/java/com/andreutp/centromasajes/model/RoleModel.java
+++ Centro-de-masajes-Relax-Relax-main/backend/centromasajes/src/main/java/com/andreutp/centromasajes/model/RoleModel.java
@@ -1,16 +1,16 @@
 package com.andreutp.centromasajes.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * Role assigned to users (e.g. ADMIN, USER, WORKER).
